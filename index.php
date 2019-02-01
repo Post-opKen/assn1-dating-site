0ea56fd5c6ca4782d9ad8b6a33c3ac5f6aceef7b
Ean Daus <eandaus123@gmail.com>
2019-02-01 15:09:17 -0500

    Added form validation and rerouting for personal info form

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ Ean Daus
 index.php
 index page for dating site
 */
+session_start();
 //php error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //require autoload
 require_once 'vendor/autoload.php';
+require_once 'model/valFunctions.php';
 
 //create an instance of the base class
 $f3 = Base::instance();
@@ -80,17 +82,96 @@ $f3->set('outdoorInterests', array('hiking', 'biking', 'swimming', 'collecting',
 
 //define a default route
 $f3->route('GET /', function () {
-    $view = new View;
+    $view = new View();
     echo $view->render('views/home.html');
 });
 
 //form templates
-$f3->route('GET|POST /personal', function () {
+$f3->route('GET|POST /personal', function ($f3) {
+    //if form has been submitted, validate data
+    if(!empty($_POST))
+    {
+        $isValid = true;
+
+        //check first/last name
+        if(!validName())
+        {
+            $isValid = false;
+            $f3->set("errors['name']", 'First and last name must contain only letters');
+        }
+
+        //check age
+        if(!validAge())
+        {
+            $isValid = false;
+            $f3->set("errors['age']", 'Age must contain only numbers and must be at least 18');
+        }
+
+        //check phone num
+        if(!validPhone())
+        {
+            $isValid = false;
+            $f3->set("errors['phone']", 'Phone number must contain only letters and have exactly 10 characters');
+        }
+
+        //check gender
+        if(isset($_POST['gender']) and $_POST['gender'] != 'male' and $_POST['gender'] != 'female')
+        {
+            $isValid = false;
+            $f3->set("errors['gender']", "pls don't spoof my form :(");
+        }
+
+        //if all data is valid
+        if($isValid)
+        {
+            //save everything to the session
+            $_SESSION['first'] = $_POST['first'];
+            $_SESSION['last'] = $_POST['last'];
+            $_SESSION['age'] = $_POST['age'];
+            $_SESSION['gender'] = $_POST['gender'];
+            $_SESSION['phone'] = $_POST['phone'];
+
+            //reroute to next page
+            $f3->reroute('profile');
+        }
+    }
     $template = new Template();
     echo $template->render('views/personal.html');
 });
 
-$f3->route('GET|POST /profile', function () {
+$f3->route('GET|POST /profile', function ($f3) {
+    //if form has been submitted, validate data
+    if(!empty($_POST))
+    {
+        $isValid = true;
+
+        //check email
+        if(!validEmail())
+        {
+            $isValid = false;
+            $f3->set("errors['email']", 'Email is invalid');
+        }
+
+        //check seeking
+        if(isset($_POST['seeking']) and $_POST['seeking'] != 'male' and $_POST['seeking'] != 'female')
+        {
+            $isValid = false;
+            $f3->set("errors['seeking']", "pls don't spoof my form :(");
+        }
+
+        //if all data is valid
+        if($isValid)
+        {
+            //save everything to the session
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['state'] = $_POST['state'];
+            $_SESSION['bio'] = $_POST['bio'];
+            $_SESSION['seeking'] = $_POST['seeking'];
+
+            //reroute to next page
+            $f3->reroute('interests');
+        }
+    }
     $template = new Template();
     echo $template->render('views/profile.html');
 });
@@ -98,6 +179,11 @@ $f3->route('GET|POST /profile', function () {
 $f3->route('GET|POST /interests', function () {
     $template = new Template();
     echo $template->render('views/interests.html');
+});
+
+$f3->route('GET|POST /summary', function () {
+    $template = new Template();
+    echo $template->render('views/summary.html');
 });
 
 //run fat free
